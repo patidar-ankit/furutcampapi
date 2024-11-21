1c6e27894ca120db3028e3039dbb092beaedd59e
Ankit Patidar <ankit.salasarcybersolution@gmail.com>
2024-11-21 10:43:02 +0530

    db upadte

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
-
+mongoose.set('strictQuery', true);
 const initMongoDB = async () => {
   try {
-    const url = process.env.MONGODB_URI;
-    const dbName = process.env.DB_NAME;
-    const mongoDB = `${url}${dbName}`;
-    await mongoose.connect(mongoDB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const username = 'doadmin';
+    const password = 'w5m2y71YKh693CF8';
+    const host = 'db-mongodb-blr1-77176-fa6e6fa7.mongo.ondigitalocean.com';
+    const database = 'admin';
+    // Construct the MongoDB URI
+    const mongoDB = `mongodb+srv://${username}:${encodeURIComponent(password)}@${host}/${database}?retryWrites=true&w=majority`;
+    // Connect to MongoDB using the latest mongoose features
+    await mongoose.connect(mongoDB);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Could not connect to MongoDB', error);
     process.exit(1);
   }
 };
-
 export default initMongoDB;
-
